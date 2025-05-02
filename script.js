4abfe46d30d190110c29f8bb0602fef85fa55de3
Yasmim Lazarim <yasslazarim@gmail.com>
2025-05-02 16:44:16 -0300

    fix mobile menu

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function openMenu(){
     var hamburguer_menu = document.querySelector(".main-navigation__hamburguer")
     hamburguer_menu.style.display = "block"
     hamburguer_menu.style.right = "0px"
-
 }
 
 function closeMenu(){
     document.querySelector(".main-navigation__hamburguer").style.display = "none"   
 }
+
+document.querySelectorAll(".main-action").forEach((action) => {
+    action.addEventListener('click', closeMenu)
+})
